99c55c958cbc5e4525e3ca921155d8fcabdc1566
bryanantonelly21 <bryanperu21@gmail.com>
2020-06-25 21:14:46 -0500

    initial commit

---
+++ js/Vick.js
@@ -0,0 +1,63 @@
+function mostrarFrases(){
+	var Frases = new Array()
+	
+Frases[0]="Adoro tu humor me cago de la risa";
+Frases[1]="Eres un genio hablando :3";
+Frases[2]="Amo tus mil y un expresiones";
+Frases[3]="Mi proximo libro te lo dedicare";
+Frases[4]="Me encanta que me inpulses a hacer cosas nuevas";
+Frases[5]="Te quiero asi aunque que te burlas de mi";
+Frases[6]="Bryan quiere que escuches https://www.youtube.com/watch?v=9FpXVBg80WM&feature=youtu.be&fbclid=IwAR3NoAJMXDrz1U-m7oy0VkxQVc8or96pkMRA2Tw6CFMeHSOibBDuzVJC__o";
+Frases[7]="Me gusta verte bailar en las fiestas";
+Frases[8]="Ya dejame cantar x1000 ctmr"
+Frases[9]="Amo cuando estas ebrio y me insultas jajajaja pdt. ya no llores rctmr";
+Frases[10]="Me siento como niña estando contigo";
+Frases[11]="Dejame cantar x3";
+Frases[12]="Dejame cantar x2";
+Frases[13]="Dejame cantar";
+Frases[14]="Odio que me hagas renegar";
+Frases[15]="Son muchisimos los recuerdos donde mis mejillas me duelen de tanto reír";
+Frases[16]="No importa el tiempo si no la calidad del mismo";
+Frases[17]="Supera a Erika x2"
+Frases[18]="Deseo que tengas el mejor año de tu vida";
+Frases[19]="Hay personas que brillan solas, y luego estas tú que iluminas a quien tocas";
+Frases[20]="Eres inteligente, Simpatico, amable y muy fuerte";
+Frases[21]="Si todo va bien, cuenta conmigo, si todo va mal... ahí estaré";
+Frases[22]="Nunca fui capaz de predecir el impacto que tendrías en mi vida";
+Frases[23]="Deja de leer mi mente jajaja";
+Frases[24]="Odiaras esta canción https://open.spotify.com/track/1qLzvvkN6OsMfqnKsUXS9q?si=cEhWpnnbRriQcqdG0OxEuA";
+Frases[25]="Esta canción me recuerda a ti https://open.spotify.com/track/7sXxvOVFtMr31xj0kX9BFV?si=wQi9LZQITaKuZade46ykqA";
+Frases[26]="Eres mi mejor amigo";
+Frases[27]="Aquí te pondre recuerdos y los abres cuando ya me hayas olvidado";
+Frases[28]="Espero conservarte un poco más como mejor amigo";
+Frases[30]="Adoro tu confianza";
+Frases[31]="Cada mentira es una deuda con la verdad, y en algún momento hay que pagarla";
+Frases[32]="Una vez eres amigo, nunca se puede dejar de serlo, te ayudaré cuando pueda y en lo que pueda y si quieres";
+Frases[33]="La mejor manera de decir te quiero es sin esperar nada a cambio";
+Frases[34]="Se que hay bonitos recuerdos, pero no es de cuerdos guardar recuerdos XD XD XD";
+Frases[35]="¿Esta empezando o va acabando nuestro tiempo como amigos?";
+Frases[36]="Frase valida por una manzana... Talvez papas fritas";
+Frases[37]="ESTE LIBRO ME LO ISNPIRASTE TU: Conocer a alguien es realmente un riesgo, ten presente que cuando dejes que ese momento pase cambiara tú vida para siempre. Ser escritor me ayudo a comprender lo trascendente que suele ser, al parecer sucede sin anticipaciones, te atrae, te atrapa y el día que estés tan sumergido, será el día te des cuenta que no hay vuelta atrás. Quiero contarles mi historia; Y no, no es una historia de romance, no hay rescate, no hay héroes y no hay una partida al atardecer, pero déjenme decirles esto: “Yo la protegería una y otra vez”.";
+Frases[38]="Escucha Iris de SWS y podrás entender un poco de mi";
+Frases[39]="Agrega una canción a la playlist que te ponga feliz con solo escucharla"
+Frases[40]="¿Ya viste el pié de página?";
+Frases[41]="Esta página fue mi intento para que sientas nuestra precencia hoy :3"
+Frases[42]="Escucha esta musica Hijo de la Luna"
+Frases[43]="Solo sigue para adelante...........me pongo reflexiva"
+Frases[44]="Te deseo lo mejor este semestre, tú puedes, solo planea las cosas, pregunta siempre"
+Frases[45]="Espero que tú y tu familia se encuentren bien en esta cuarentena"
+Frases[46]="No dejes de ser como eres"
+Frases[47]="Puedes ver todo el codigo fuente, en especial para que veas estas frases a detalle :3"
+Frases[48]="Supera a Erika"
+Frases[49]="010101010 10101 010101 11010101 1001 01010 1010101 01110001 000101010"
+Frases[50]="JAJAJAJJAJA el mensaje en binario es random nose ni yo que puse"
+Frases[51]="Eres un monse y una vaca lechera"
+
+
+var Q = Frases.length
+var numAleatorio = Math.round(Math.random()*(Q-1));
+	swal('Assly te dice:',Frases[numAleatorio]);
+
+}
+
+
